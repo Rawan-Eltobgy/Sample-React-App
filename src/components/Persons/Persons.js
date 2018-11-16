7ec62bd9780f8660e31b31032d613402def50d75
Eltobgy <rawaneltobgy@gmail.com>
2018-11-16 23:14:50 +0200

    Converting from function components to class components

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 //Functional component cause we have no need to manage state in there
-const persons = (props) => props.persons.map( ( person, index ) => {
-  //props.person.map is normal js code not a jsx
-        return <Person
-          click={() => props.clicked( index )}
-          name={person.name}
-          age={person.age}
-          key={person.id}
-          changed={( event ) => props.changed( event, person.id )} />
-      } );
+class Persons extends Component {
+render(){
+  return this.props.persons.map((person, index) => {
+    //props.person.map is normal js code not a jsx
+    return <Person
+      click={() => this.props.clicked(index)}
+      name={person.name}
+      age={person.age}
+      key={person.id}
+      changed={(event) => this.props.changed(event, person.id)} />
+  });
+}}
 
-export default persons;
+
+export default Persons;
